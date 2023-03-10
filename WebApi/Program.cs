2ca29e325796b0b2c27388976c287e6dda73b3fe
Kinjal Chaudhary <kinjal@kidly.co.uk>
2023-03-10 10:26:42 +0000

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DbConnection"));
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
